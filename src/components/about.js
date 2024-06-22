5871c5b26fa86f23ad1b62f37fa943e6a57b8fe3
achuehuehue <akanshachouhan2004.06@gmail.com>
2024-06-23 04:02:15 +0530

    Fixed about page UI

--- src/components/about.js
+++ src/components/about.js
@@ -1,21 +1,27 @@
 const About=()=>{
     return (
-        <div>
-            <p className="p-4 m-4 border-b-2 font-bold text-xl">Welcome to YumRush!
-             YumRush is your ultimate online destination for discovering and ordering from the
-             finest restaurants in Hyderabad. Our platform is designed to cater to your culinary
-             cravings with ease and convenience, offering a diverse range of food options from 
-             top-rated eateries across the city. Whether you’re in the mood for traditional Hyderabadi
-             biryani, exotic international cuisines, or a simple, comforting meal, YumRush has you covered.</p>
+        <div className="w-[100%] h-screen bg-gray-100">
+            <div className="w-[100%] flex flex-col justify-center items-center bg-gray-100">
+                <div className="text-[114%] border-b-2 h-[50%] text-justify w-[85%] pb-4 mt-32 bg-gray-100">
+                    Welcome to <span className="text-orange-500 font-bold">YumRush! </span>
+                    Your ultimate online destination for discovering and ordering from the
+                    finest restaurants in Hyderabad. Our platform is designed to cater to your culinary
+                    cravings with ease and convenience, offering a diverse range of food options from 
+                    top-rated eateries across the city. Whether you’re in the mood for traditional Hyderabadi
+                    biryani, exotic international cuisines, or a simple, comforting meal, YumRush has you covered.
+                </div>
 
-             <p className="m-4 p-4 font-bold text-xl">Explore a vast array of restaurants on YumRush, each offering a unique dining experience. 
-             Our user-friendly interface allows you to browse through various eateries, check ratings, 
-             and view their specialty dishes. From upscale fine dining to cozy neighborhood cafes, 
-             YumRush brings you closer to the best of Hyderabad's culinary scene.</p>
-             <div className=" p-4 bg-black text-white">
-                <p className="py-[4%] px-[44%]">Made by Akansha</p>
-             </div>
-        </div>
-    )
+                <div className="text-lg text-justify h-[50%] w-[85%] mt-4 mb-[110px] bg-gray-100">
+                    Explore a vast array of restaurants on YumRush, each offering a unique dining experience. 
+                    Our user-friendly interface allows you to browse through various eateries, check ratings, 
+                    and view their specialty dishes. From upscale fine dining to cozy neighborhood cafes, 
+                    YumRush brings you closer to the best of Hyderabad's culinary scene.
+                </div> 
+            </div>
+            <div className="collapse md:visible flex items-center justify-center p-4 bg-black text-white w-[100%] h-[150px] fixed bottom-0">
+                <p className="">Made by Akansha</p>
+            </div>
+        </div>    
+        )
 }
 export default About;
