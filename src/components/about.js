8eb1f61a7471406fcbe2806139ed59786899458a
achuehuehue <akanshachouhan2004.06@gmail.com>
2024-07-03 00:28:51 +0530

    Updated about us with terms and conditions

--- src/components/about.js
+++ src/components/about.js
@@ -11,11 +11,40 @@ const About=()=>{
                     biryani, exotic international cuisines, or a simple, comforting meal, YumRush has you covered.
                 </div>
 
-                <div className="text-lg text-justify h-[50%] w-[85%] mt-4 mb-[110px] bg-gray-100">
+                <div className="text-lg text-justify h-[50%] w-[85%] mt-4 bg-gray-100">
                     Explore a vast array of restaurants on YumRush, each offering a unique dining experience. 
                     Our user-friendly interface allows you to browse through various eateries, check ratings, 
                     and view their specialty dishes. From upscale fine dining to cozy neighborhood cafes, 
                     YumRush brings you closer to the best of Hyderabad's culinary scene.
+                </div> 
+
+                <div className="text-lg text-justify h-[50%] w-[85%] mt-5 mb-[110px] bg-gray-100">
+                  <p className="font-bold text-xl mb-4">Terms and Conditions</p>
+                  <p className="font-bold mb-1">Introduction</p>
+                  By using YumRush, you agree to these terms and conditions. The content on this site is for your general information and use only and may change without notice.
+                  <p className="font-bold mt-4 mb-1">Use of Site</p>
+                  You use the site at your own risk. Unauthorized use may result in legal action.
+                  <p className="font-bold text-xl mt-6 mb-4">Privacy Policys</p>
+                  <p className="font-bold mb-1">Data Collection</p>
+                  We collect personal information and usage data to enhance our service.
+                  <p className="font-bold mt-4 mb-1">Data Use</p>
+                  Your data helps us process orders, improve our services, and communicate with you.
+                  <p className="font-bold mt-4 mb-1">Data Protection</p>
+                  We use security measures to protect your personal information.
+                  <p className="font-bold text-xl mt-6 mb-4">Refunds/Cancellations Policy</p>
+                  <p className="font-bold mb-1">Refunds</p>
+                  You can request a refund within 1 day of purchase for eligible orders. Refunds will be processed within 5-7 working days.
+                  <p className="font-bold mt-4 mb-1">Cancellations</p>
+                  Orders can be cancelled within 3 minutes of placing the order. Contact customer support for cancellations.
+                  <p className="font-bold text-xl mt-6 mb-4">Shipping Policy</p>
+                  <p className="font-bold mb-1">Delivery Times</p>
+                  The minimum and maximum delivery times can vary based on factors such as location, traffic, kitchen preparation time, and delivery distance.
+                  <p className="font-bold mt-4 mb-1">Charges</p>
+                  Delivery charges are calculated based on distance.
+                  <p className="font-bold text-xl mt-6 mb-4">Contact Us</p>
+                  <p><strong>Email: </strong>akanshachouhan2004.06@gmail.com</p>
+                  <p><strong>Phone:</strong> 9693164695</p>
+                  <strong>Address:</strong> Sail City, Ranchi, Jharkhand - 834004
                 </div> 
             </div>
             <div className="collapse md:visible flex items-center justify-center p-4 bg-black text-white w-[100%] h-[150px] fixed bottom-0">
